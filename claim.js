8ccaa9c65299ab76d0ffc71eee45151df763782e
sebushi113 <sebushi113@users.noreply.github.com>
2022-09-28 03:35:32 -0400

    gitignore update

--- claim.js
+++ claim.js
@@ -34,7 +34,6 @@ function sleep(ms) {
 async function cs1_claim_rplanet() {
   // while (true) {
   try {
-    // console.log(new Date() + "\nclaiming...");
     const transaction = await api.transact(
       {
         actions: [
@@ -50,16 +49,11 @@ async function cs1_claim_rplanet() {
       },
       { blocksBehind: 3, expireSeconds: 30 }
     );
-    // console.log(`> 🦁   RP claimed  🐷 | ${moment(new Date()).format(date)}`); //🦁
     console.log(
-      `  🦁   | ${moment(new Date()).format(date)}   ${
+      `  🦁   | ${moment(new Date()).format(date)} | ${
         transaction.transaction_id
       }`
     );
-    // console.log(
-    //   `https://wax.bloks.io/transaction/${transaction.transaction_id}`
-    // );
-    // console.log("- 🦁   RP verifying..."); //🆚
     setTimeout(() => {
       cs1_claim_rplanet();
     }, 5000);
@@ -86,7 +80,6 @@ async function cs1_claim_rplanet() {
 
 async function cd3_claim_rplanet() {
   try {
-    // console.log(Date() + "\nclaiming...");
     const transaction = await api.transact(
       {
         actions: [
@@ -102,16 +95,11 @@ async function cd3_claim_rplanet() {
       },
       { blocksBehind: 3, expireSeconds: 30 }
     );
-    // console.log(`> 🐵   RP claimed  🐷 | ${moment(new Date()).format(date)}`); //🦁
     console.log(
       `  🐵   | ${moment(new Date()).format(date)} | ${
         transaction.transaction_id
       }`
     );
-    // console.log(
-    //   `https://wax.bloks.io/transaction/${transaction.transaction_id}`
-    // );
-    // console.log("- 🐵   RP verifying..."); //🆚
     setTimeout(() => {
       cd3_claim_rplanet();
     }, 5000);
@@ -138,7 +126,6 @@ async function cd3_claim_rplanet() {
 async function all_claim_greenrabbit() {
   // while (true) {
   try {
-    // console.log(Date() + "\nclaiming staking.gr...");
     const transaction = await api.transact(
       {
         actions: [
@@ -172,13 +159,11 @@ async function all_claim_greenrabbit() {
       },
       { blocksBehind: 3, expireSeconds: 30 }
     );
-    // console.log(`> 🦁🐵 GR claimed  🐰 | ${moment(new Date()).format(date)}`); //🦁
     console.log(
       ` 🦁🐵 | ${moment(new Date()).format(date)} | ${
         transaction.transaction_id
       }`
     );
-    // console.log("- 🦁🐵 GR verifying...");
     setTimeout(() => {
       all_claim_greenrabbit();
     }, 5000);
@@ -188,7 +173,6 @@ async function all_claim_greenrabbit() {
       "assertion failure with message: nothing to claim just yet"
     ) {
       console.log(" ✅✅ | nothing to claim, waiting...");
-      // console.log("- 🦁🐵 GR trying to claim again at 17:03:00..."); //⏩
     } else {
       setTimeout(() => {
         console.log(` 🦁🐵 | ${moment(new Date()).format(date)} | error`);
@@ -212,156 +196,3 @@ console.log("  🐵   | waiting to claim on min 3 of even hour...");
 
 cron.schedule("0 17 * * */1", all_claim_greenrabbit);
 console.log(" 🦁🐵 | waiting to claim at 17:00:00...");
-
-// console.log(
-//   `🦁🐵 | ${moment(new Date()).format(
-//     date
-//   )} | 6ac208e08d2a636824124d7556087e23e9076f8fbc6329b1d8fca40cd732b3a6`
-// ); //🆔
-
-// cd3_claim_greenrabbit();
-// cs1_claim_greenrabbit();
-// cron.schedule("0 17 * * */1", cs1_claim_greenrabbit);
-// console.log("~ 🦁 GR 🐰 waiting to claim at 17:00:00...");
-// cron.schedule("0 17 * * */1", cd3_claim_greenrabbit);
-// console.log("~ 🐵 GR 🐰 waiting to claim at 17:00:00...");//🦋
-
-// async function test() {
-//   // let date = moment(new Date()).format(date);
-
-//   // let date = moment(new Date());
-//   // let current_date = date.format(date);
-//   console.log(`> GR claimed  🐰 | ${moment(new Date()).format(date)}`); //🦁
-//   await sleep(2000);
-//   console.log(`> GR claimed  🐰 | ${moment(new Date()).format(date)}`); //🦁
-//   // console.log(mmnt() + " 00 nothing to claim ✅");
-//   // console.log(new Date().toISOString("en-US"));
-//   // console.log(mmnt() + " 01 nothing to claim ✅");
-//   // await sleep(2000);
-//   // console.log(mmnt() + " 02 nothing to claim ✅");
-// }
-
-// test();
-
-// let date = moment(new Date());
-// function mmnt() {
-//   let current_date = date.format(date);
-// console.log(current_date);
-// }
-
-// setTimeout(function () {
-//   console.log(mmnt);
-// }, 2000);
-// setTimeout(console.log(mmnt), 2000);
-
-// setTimeout(function () {
-//   let date = moment(new Date());
-//   let current_date = date.format(date);
-//   console.log(`${current_date} | 01 nothing to claim ✅`);
-// }, 2000);
-
-// setTimeout(function () {
-//   let current_date = date.format(date);
-//   console.log(`${current_date} | 02 nothing to claim ✅`);
-// }, 2000);
-
-// function date(x) {
-//   moment(x).format("YYYY-MM-DD HH:mm:ss");
-// }
-
-// console.log("> GR claimed  🐷 | 2022>09-25 12:03:01");
-// console.log("> GR trying to claim again at 17:03:00..."); //⏩
-
-// async function cs1_claim_greenrabbit() {
-//   // while (true) {
-//   try {
-//     // console.log(Date() + "\nclaiming staking.gr...");
-//     const transaction = await api.transact(
-//       {
-//         actions: [
-//           {
-//             account: "staking.gr",
-//             name: "claim",
-//             authorization: [{ actor: cs1, permission: cs1_perm }],
-//             data: {
-//               user: cs1,
-//             },
-//           },
-//           {
-//             account: "driveless.gr",
-//             name: "claim",
-//             authorization: [{ actor: cs1, permission: cs1_perm }],
-//             data: {
-//               user: cs1,
-//               collection: "greenrabbit",
-//             },
-//           },
-//         ],
-//       },
-//       { blocksBehind: 3, expireSeconds: 30 }
-//     );
-//     console.log(`> 🦁 GR claimed  🐰 | ${moment(new Date()).format(date)}`); //🦁
-//     console.log(`- 🦁 GR ${transaction.transaction_id}`);
-//     console.log("- 🦁 GR verifying..."); //🦋
-//     setTimeout(() => {
-//       cs1_claim_greenrabbit();
-//     }, 5000);
-//   } catch (error) {
-//     if (
-//       error.message ==
-//       "assertion failure with message: nothing to claim just yet"
-//     ) {
-//       console.log("- 🦁 GR nothing to claim ✅");
-//       console.log("- 🦁 GR trying to claim again at 17:03:00..."); //⏩
-//     } else {
-//       setTimeout(() => {
-//         console.log(`* 🦁 GR ${moment(new Date()).format(date)} | ${error}`);
-//         cs1_claim_greenrabbit();
-//       }, 10000);
-//     }
-//     // }
-//   }
-// }
-
-// async function cd3_claim_greenrabbit() {
-//   // while (true) {
-//   try {
-//     // console.log(Date() + "\nclaiming staking.gr...");
-//     const transaction = await api.transact(
-//       {
-//         actions: [
-//           {
-//             account: "staking.gr",
-//             name: "claim",
-//             authorization: [{ actor: cd3, permission: cd3_perm }],
-//             data: {
-//               user: cd3,
-//               collection: "greenrabbit",
-//             },
-//           },
-//         ],
-//       },
-//       { blocksBehind: 3, expireSeconds: 30 }
-//     );
-//     console.log(`> 🐵 GR claimed  🐰 | ${moment(new Date()).format(date)}`); //🦁
-//     console.log(`- 🐵 GR ${transaction.transaction_id}`);
-//     console.log("- 🐵 GR verifying..."); //🦋
-//     setTimeout(() => {
-//       cd3_claim_greenrabbit();
-//     }, 5000);
-//   } catch (error) {
-//     if (
-//       error.message ==
-//       "assertion failure with message: nothing to claim just yet"
-//     ) {
-//       console.log("- 🐵 GR nothing to claim ✅");
-//       console.log("- 🐵 GR trying to claim again at 17:03:00..."); //⏩
-//     } else {
-//       setTimeout(() => {
-//         console.log(`* 🐵 GR ${moment(new Date()).format(date)} | ${error}`);
-//         cd3_claim_greenrabbit();
-//       }, 10000);
-//     }
-//     // }
-//   }
-// }
